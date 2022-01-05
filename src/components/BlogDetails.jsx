ce9039dc13f8c2707761528df8004d4430dfa4e5
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2022-01-05 18:03:47 -0500

    added onclick ability to blog list
     - clicking on a blog summary will route a user to the relevant blog
     - will route the user to the blog's full view itself.

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,12 +1,30 @@
 import {useParams} from "react-router-dom";
-import {useEffect,useState} from 'react';
+import {useEffect, useState} from 'react';
 
-export default function BlogDetails({blogs}){
-
+export default function BlogDetails({blogs}) {
+    const [dislayBlog, updateDisplayBlog] = useState(null);
     const {id} = useParams();
+
+    function findBlog() {
+        return blogs.find(blog => blog.id === parseInt(id));
+    }
+    const blog = findBlog();
+
+
     return (
         <div>
-            <h3>You were searching for blog: {id}</h3>
+            {!blog ? <h3>That blog does not exist</h3> : ""}
+            <div className="container blog__card">
+                <div className="blog__title">
+                    {blog.title}
+                </div>
+                <div className="blog__username">
+                    {blog.username}
+                </div>
+                <div className="blog__body">
+                    {blog.content}
+                </div>
+            </div>
         </div>
     )
 }
