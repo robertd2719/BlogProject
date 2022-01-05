3a01ce0b742afd67d3519d820eb9fd095f280bc2
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2022-01-05 02:17:29 -0500

    fixed datetimeFormatter for blog date stamp.
    - onsubmit now correctly parses date object and returns correct string for JSON object and database record

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -1,34 +1,53 @@
-import {useState,useEffect} from 'react';
+import {useState} from 'react';
 
 
-export default function AddBlog({blog}){
+export default function AddBlog({blog}) {
     const [username, setUserName] = useState('');
     const [title, setTitle] = useState('');
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
 
-    function formattedDatetime(date){
+    function formattedDate(date) {
+        const year = date.getFullYear();
+        const month = date.getMonth()<10 ? `0${date.getMonth()+1}` : date.getMonth+1;
+        const day = date.getDay()<10 ? `0${date.getDay()}` : date.getDay()
+        return `${year}-${month}-${day}`;
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const date = formattedDate(new Date())
+        setUserName('');
+        setTitle('');
+        setSummary('');
+        setContent('');
+        alert(`Date: ${date}`);
 
     }
 
-    return(
+    return (
         <div>
-            <form action="" className="new__blog__form">
+            <form onSubmit={e => handleSubmit(e)} className="new__blog__form">
                 <div className="form__field">
                     <label htmlFor="username">USER: </label>
-                    <input type="text" id="username" value={username} onChange={e=>setUserName(e.target.value)}/>
+                    <input type="text" id="username" value={username} onChange={e => setUserName(e.target.value)}/>
                 </div>
                 <div className="form__field">
                     <label htmlFor="title">TITLE: </label>
-                    <input type="text" id="title" value={title} onChange={e=>setTitle(e.target.value)}/>
+                    <input type="text" id="title" value={title} onChange={e => setTitle(e.target.value)}/>
                 </div>
                 <div className="form__field">
                     <label htmlFor="summary">SUMMARY: </label>
-                    <input type="text" name="summary" id="summary"/>
+                    <input type="text" name="summary" id="summary" value={summary}
+                           onChange={e => setSummary(e.target.value)}/>
                 </div>
                 <div className="form__field">
                     <label htmlFor="content">CONTENT:</label>
-                    <textarea name="content" id="content" cols="54" rows="10"></textarea>
+                    <textarea name="content" id="content" cols="54" rows="10" value={content}
+                              onChange={e => setContent(e.target.value)}/>
+                </div>
+                <div className="form__field">
+                    <input type="submit" value="submit"/>
                 </div>
             </form>
         </div>
